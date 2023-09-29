6a3c94bfd7db6fa56a7a05ece340fd39df1a1a63
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2023-09-29 14:51:41 +0200

    egzamin

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,29 +1,45 @@
-﻿namespace domekWgorachBadowski4c;
+﻿namespace _29._09._23Badowski4c;
 
 public partial class MainPage : ContentPage
 {
-
 	int x = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
-
 	}
 
-	private void dodaj(object sender, EventArgs e)
+	private void zmienWielkosc(object sender, ValueChangedEventArgs e)
 	{
-		x++;
-		polubienia.Text = x + " polubień";
+		double wielkosc = 0;
+		wielkosc = slider1.Value;
+		tuRozmiar.Text = "Rozmiar: " + Math.Round(wielkosc, 0);
+		wynik.FontSize = wielkosc;
 	}
-    private void odejmij(object sender, EventArgs e)
-    {
-        x--;
-		if(x < 0)
+	private void zmien(object sender, EventArgs e)
+	{
+        string[] napisy;
+        napisy = new string[3];
+        napisy[0] = "Good morning";
+        napisy[1] = "Buenos dias";
+        napisy[2] = "Dzień dobry";
+
+		if(x > 2)
 		{
 			x = 0;
 		}
-        polubienia.Text = x + " polubień";
+
+		if (x == 0)
+		{
+			wynik.Text = napisy[0];
+		} else if (x == 1)
+		{
+            wynik.Text = napisy[1];
+        } else if (x == 2)
+		{
+            wynik.Text = napisy[2];
+        }
+		x++;
     }
 }
 
